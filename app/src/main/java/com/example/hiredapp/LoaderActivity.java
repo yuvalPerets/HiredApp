c9d8a3cf8e4a04493aa89d783c92469f6c794b54
HilaMasturov <Hila31097@gmail.com>
2024-06-14 20:33:48 +0300

    loader page

---
+++ app/src/main/java/com/example/hiredapp/LoaderActivity.java
@@ -0,0 +1,42 @@
+
+package com.example.hiredapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_loader);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loader_layout), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    synchronized (this) {
+                        wait(2000);
+                        Intent intent = new Intent (LoaderActivity.this,MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
